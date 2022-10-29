6dc8059a697b9efb243543abb371929766a1fc24
Luis Stumpf <trumpf25lp@gmail.com>
2022-10-29 22:55:49 +0200

    colemak

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -2,19 +2,30 @@
 local opts = { noremap = true, silent = true }
 
 
+
+
+
+
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
+
 
 --Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- navigation colemak
+keymap("n", "n", "h", opts)
+keymap("n", "e", "j", opts)
+keymap("n", "i", "k", opts)
+keymap("n", "o", "l", opts)
+keymap("n", "f", "i", opts)
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-n>", "<C-w>h", opts)
+keymap("n", "<C-e>", "<C-w>j", opts)
+keymap("n", "<C-i>", "<C-w>k", opts)
+keymap("n", "<C-o>", "<C-w>l", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
